e5e16b3a87beb25c1e9ee647c9a53c3061f0c126
dhawal19 <95870401+dhawal19@users.noreply.github.com>
2022-01-13 23:56:54 +0530

    Add files via upload

---
+++ Output_assignment_1.py
@@ -0,0 +1,32 @@
+Python 3.8.10 (tags/v3.8.10:3d8993a, May  3 2021, 11:48:03) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+============ RESTART: C:\Users\asus\OneDrive\Desktop\assignment 1.py ===========
+enter first number: 65
+enter second number: 22
+enter third number: 64
+The average is:  50.333333333333336 
+
+Enter the Gross income $ 100000
+Enter number of dependents 3
+Tax is:  16200.0 $ 
+
+Enter the SID of the student: 21103034
+Enter the name of the student: Dhawal Arora
+Enter the gender of he student (use F, M or U for unknown): M
+Enter the course the student id enrolled in: CSE
+Enter the CGPA of the student: 8.0
+The details of the student are:  ['21103034', 'Dhawal Arora', 'M', 'CSE', 8.0] 
+
+Enter the marks of student 1: 88
+Enter the marks of student 2: 56
+Enter the marks of student 3: 23
+Enter the marks of student 4: 45
+Enter the marks of student 5: 98
+The marks of the students are:  [23, 45, 56, 88, 98] 
+
+The new list is  ['Red', 'Green', 'White', 'Pink', 'Yellow'] 
+
+The edited list is  ['Red', 'Green', 'White', 'Purple', 'Yellow'] 
+
+>>> 
